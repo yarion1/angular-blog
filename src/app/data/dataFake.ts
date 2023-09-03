a6af87d562f2405dd4e71bcac2591efe40e8e80c
yarion1 <pablo.dmg@outlook.com>
2023-09-03 14:23:30 -0300

    refactoring and adding css features

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Mr. Robot season 2 is back",
+        "description": "Forget the story, characters, or acting, the first season of Mr. Robot grabbed attention because of its incredibly accurate portrayal of hacking. Like the rest of us, show creator Sam Esmail must have had enough of the kind of nonsense computer scenes littering the likes of CSI Cyber and Swordfish, so he hired help in the form of ex-hackers and former FBI investigators as consultants on the Anonymous-themed show.",
+        "photoCover": "https://media.wired.co.uk/photos/606db6fc938ecee6e930f5fa/master/w_1600%2Cc_limit/mr-robot-season-2-poster.jpg"
+    },
+    {
+        "id": "2",
+        "title": "FSociety wants a piece of the ransomware cake",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae al",
+        "photoCover": "https://www.webassetscdn.com/avira/prod-blog/wp-content/uploads/2016/08/avira_blog_mr.robot-header.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Jerry / fsociety · GitLab",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae al",
+        "photoCover": "https://user-content.gitlab-static.net/fc5dfb6297f2fd3ef99c10fffd16b75de51bcace/68747470733a2f2f61736369696e656d612e6f72672f612f55526a326e7670625970654a794a6534334b6c41535a37667a2e706e67"
+    },
+    {
+        "id": "4",
+        "title": "Fsociety designs, themes, templates and downloadable graphic elements",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae al",
+        "photoCover": "https://cdn.dribbble.com/users/2158940/screenshots/6721987/darkarmy.png?resize=400x0"
+    }
+
+]
